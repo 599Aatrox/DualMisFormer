982c731904aa6208b96bc288b9ecb5907fdad42d
11731 <1173158108@qq.com>
2025-12-10 14:52:59 +0800

    使用了交叉注意力机制

--- models/Client.py
+++ models/Client.py
@@ -13,15 +13,16 @@ from layers.SelfAttention_Family import FullAttention, AttentionLayer, ProbAtten
 from layers.Embed import DataEmbedding, DataEmbedding_inverted
 import numpy as np
 from layers.RevIN import RevIN
+
 # 创建 logs 目录（如果不存在）
 os.makedirs("logs", exist_ok=True)
 
 # 配置日志
 logging.basicConfig(
-    filename='logs/experiment.log',          # 日志文件路径
-    filemode='a',                            # 追加模式（'w' 会覆盖）
+    filename='logs/experiment.log',  # 日志文件路径
+    filemode='a',  # 追加模式（'w' 会覆盖）
     format='%(asctime)s - %(levelname)s - %(message)s',
-    level=logging.INFO                      # 只记录 INFO 及以上级别
+    level=logging.INFO  # 只记录 INFO 及以上级别
 )
 logger = logging.getLogger()
 warnings.filterwarnings('ignore')
@@ -83,7 +84,8 @@ class CrossModalAttentionFusion(nn.Module):
         channel_to_phase, _ = self.cross_attn_c2p(
             phase_emb,  # Query
             channel_emb,  # Key
-            channel_emb  # Value
+            channel_emb,  # Value
+            attn_mask=None
         )
         channel_to_phase = self.norm1(channel_emb + channel_to_phase)
 
@@ -91,7 +93,8 @@ class CrossModalAttentionFusion(nn.Module):
         phase_to_joint, _ = self.cross_attn_p2j(
             joint_emb,
             phase_emb,
-            phase_emb
+            phase_emb,
+            attn_mask=None
         )
         phase_to_joint = self.norm2(phase_emb + phase_to_joint)
 
@@ -99,7 +102,8 @@ class CrossModalAttentionFusion(nn.Module):
         joint_to_channel, _ = self.cross_attn_j2c(
             channel_emb,
             joint_emb,
-            joint_emb
+            joint_emb,
+            attn_mask=None
         )
         joint_to_channel = self.norm3(joint_emb + joint_to_channel)
 
@@ -115,11 +119,13 @@ class CrossModalAttentionFusion(nn.Module):
         fused_final, _ = self.self_attn(
             fused_proj,
             fused_proj,
-            fused_proj
+            fused_proj,
+            attn_mask=None
         )
         fused_final = self.norm_final(fused_proj + fused_final)
 
         return fused_final
+
 
 class Model(nn.Module):
 
@@ -128,7 +134,7 @@ class Model(nn.Module):
         self.pred_len = configs.pred_len
         self.output_attention = configs.output_attention
         # print(configs.d_model)
-        #todo: d_model 最好是256但是作者给直接和seq_len一样了
+        # todo: d_model 最好是256但是作者给直接和seq_len一样了
         # configs.d_model = configs.seq_len
 
         self.seq_len = configs.seq_len
@@ -153,11 +159,11 @@ class Model(nn.Module):
             ],
             norm_layer=torch.nn.LayerNorm(configs.d_model)
         )
-        #通道嵌入
+        # 通道嵌入
         self.channel_embedding = nn.Parameter(torch.zeros(configs.enc_in, configs.d_model))
-        self.phase_embedding = nn.Embedding(self.cycle_len,configs.d_model)
+        self.phase_embedding = nn.Embedding(self.cycle_len, configs.d_model)
         nn.init.xavier_normal(self.phase_embedding.weight)
-        self.joint_embedding = nn.Embedding(self.cycle_len,self.enc_in*self.d_model)
+        self.joint_embedding = nn.Embedding(self.cycle_len, self.enc_in * self.d_model)
         nn.init.xavier_normal(self.joint_embedding.weight)
         nn.init.xavier_normal(self.channel_embedding)
 
@@ -173,7 +179,7 @@ class Model(nn.Module):
         self.Linear = nn.Linear(self.seq_len, self.seq_len)
         self.GeLU = nn.GELU()
         self.Hidden1 = nn.Linear(self.seq_len, self.pred_len)
-        self.w_dec = torch.nn.Parameter(torch.FloatTensor([configs.w_lin]*configs.enc_in),requires_grad=True)
+        self.w_dec = torch.nn.Parameter(torch.FloatTensor([configs.w_lin] * configs.enc_in), requires_grad=True)
         self.revin_layer = RevIN(configs.enc_in)
         self.log_prem = True
         self.use_L = configs.use_L
@@ -183,52 +189,55 @@ class Model(nn.Module):
             dropout=configs.dropout
         )
 
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, phase,
+                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None, ):
 
-    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec,phase,
-                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None,):
-
-        x_enc = self.revin_layer(x_enc, 'norm')#归一化
-        B,L,N = x_enc.shape
-
+        x_enc = self.revin_layer(x_enc, 'norm')  # 归一化
+        B, L, N = x_enc.shape
 
         # enc_out = x_enc.permute(0, 2, 1)#[batch_size,enc_in,seq_len]，[32,7,96]
-        enc_out = self.enc_embedding(x_enc, x_mark_enc)#[batch_size,enc_in,seq_len]，[32,7,96]
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)  # [batch_size,enc_in,seq_len]，[32,7,96]
         if self.log_prem:
-            logger.info("B,L,N", B, L, N)
-            logger.info("enc_out", enc_out.shape)
+            logger.info("B,L,N, {B}, {L}, {N}")
+            logger.info("enc_out, {enc_out.shape}")
 
         channel_emb = self.channel_embedding.expand(enc_out.shape[0], N, -1)
 
         phase_emb = self.phase_embedding(phase.view(-1, 1).expand(B, N))
         joint_emb = self.joint_embedding(phase).reshape(B, self.enc_in, self.d_model)
-        # enc_out = enc_out[:, :N, :] + channel_emb + phase_emb + joint_emb  # [B,N,],[32, 7, 256]
-        fused_emb = self.cross_attention(channel_emb, phase_emb, joint_emb)
-        enc_out = enc_out[:, :N, :] + fused_emb
+
+        if self.use_L:
+            fused_emb = self.cross_attention(channel_emb, phase_emb, joint_emb)
+            enc_out = enc_out[:, :N, :] + fused_emb
+        else:
+            enc_out = enc_out[:, :N, :] + channel_emb + phase_emb + joint_emb  # [B,N,],[32, 7, 256]
         if self.log_prem:
-            logger.info("channel_emb", channel_emb.shape)
-            logger.info("phase_emb", phase_emb.shape)
-            logger.info("joint_emb", joint_emb.shape)
-            logger.info("enc_out", enc_out.shape)
+            logger.info("channel_emb, {channel_emb.shape}")
+            logger.info("phase_emb, {phase_emb.shape}")
+            logger.info("joint_emb, {joint_emb.shape}")
+            logger.info("enc_out, {enc_out.shape}")
         enc_orgin = enc_out
-        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)#[batch_size,enc_in,seq_len],[32, 7, 96]
+        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)  # [batch_size,enc_in,seq_len],[32, 7, 96]
 
         dec_out = self.projector(enc_out + enc_orgin).permute(0, 2, 1)[:, :, :N]
         if self.log_prem:
-            logger.info("dec_out", dec_out.shape)
-        if self.use_L:
-            x = x_enc.permute(0,2,1)
+            logger.info("dec_out, {dec_out.shape}")
+        if 1:
+            x = x_enc.permute(0, 2, 1)
             x3 = self.Linear(x)
             x3 = self.GeLU(x3)
             x3 = self.Hidden1(x3)
-            linear_out= x3.permute(0,2,1)
-            dec_out = self.revin_layer(dec_out[:, -self.pred_len:, :]+self.w_dec*linear_out, 'denorm')#[batch_size,seq_len,enc_in][32, 96, 7]
+            linear_out = x3.permute(0, 2, 1)
+            dec_out = self.revin_layer(dec_out[:, -self.pred_len:, :] + self.w_dec * linear_out,
+                                       'denorm')  # [batch_size,seq_len,enc_in][32, 96, 7]
             if self.log_prem:
-                logger.info("dec_out", dec_out.shape)
-                logger.info("linear_out", linear_out.shape)
+                logger.info("dec_out, {dec_out.shape}")
+                logger.info("linear_out, {linear_out.shape}")
         else:
-            dec_out = self.revin_layer(dec_out[:, -self.pred_len:, :], 'denorm')#[batch_size,seq_len,enc_in][32, 96, 7]
+            dec_out = self.revin_layer(dec_out[:, -self.pred_len:, :],
+                                       'denorm')  # [batch_size,seq_len,enc_in][32, 96, 7]
             if self.log_prem:
-                logger.info("dec_out", dec_out.shape)
+                logger.info("dec_out, {dec_out.shape}")
         self.log_prem = False
         if self.output_attention:
             return dec_out[:, -self.pred_len:, :], attns
